2b2460360f3b0c8e40020610e049051af6fbec69
Administrator <admin@tradicaosjc.com.br>
2026-01-07 22:58:19 -0300

    fix: Corrige timezone do PDF de bipagens pendentes para horário do Brasil

--- packages/backend/src/services/bip-pdf.service.ts
+++ packages/backend/src/services/bip-pdf.service.ts
@@ -114,8 +114,10 @@ export class BipPDFService {
               timeZone: 'America/Sao_Paulo',
               day: '2-digit',
               month: '2-digit',
+              year: 'numeric',
               hour: '2-digit',
-              minute: '2-digit'
+              minute: '2-digit',
+              second: '2-digit'
             })
           : '-';
 
